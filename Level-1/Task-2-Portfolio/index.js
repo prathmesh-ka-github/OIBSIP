12ffbeaec4887ab048005a3ff4c42775e86ed4fe
Prathmesh <connect.prathmesh905@gmail.com>
2022-12-03 15:26:26 +0530

    ham menu fixed

--- Level-1/Task-2-Portfolio/index.js
+++ Level-1/Task-2-Portfolio/index.js
@@ -1,9 +1,11 @@
 function ham() {
     var menu = document.getElementById("ham-menu-id");
     if (menu.style.opacity === '1'){
+        menu.style.height = '0'
         menu.style.opacity = '0';
     }
     else {
+        menu.style.height = 'auto'
         menu.style.opacity = '1';
     }
 }
